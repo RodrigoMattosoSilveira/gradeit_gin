40b80c3d76dc671aee5fe8ef033575c95041fc34
Rodrigo Silveira <r3850365@gmail.com>
2024-05-23 16:42:41 -0700

    Fixed Issue #3

--- src/main.go
+++ src/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+
+	"madronetek.com/gradeit/config"
+)
+
+func init() {
+	config.LoadEnv()
+}
 
 func main() {
- 	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H {
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
 }
